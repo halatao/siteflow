c163528fb2c7b57e4a80e1f608229ba49aacf8d1
halatao <99823777+halatao@users.noreply.github.com>
2026-06-10 10:36:14 +0200

    Export merged component catalog

--- src/catalog/index.ts
+++ src/catalog/index.ts
@@ -1,4 +1,5 @@
 export * from "./component-roles.js";
 export * from "./website-archetypes.js";
-export * from "./component-definitions.js";
+export type { ComponentDefinition, ComponentImplementationStatus } from "./component-definitions.js";
+export { componentDefinitions, componentDefinitionIds } from "./component-catalog.js";
 export * from "./block-prop-schemas.js";
